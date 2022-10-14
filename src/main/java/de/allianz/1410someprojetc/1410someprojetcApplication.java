ebcc0b03ae56d97de362406948e7ba921c861a78
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-14 12:59:59 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/1410someprojetc/1410someprojetcApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.1410someprojetc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 1410someprojetcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(1410someprojetcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/1410someprojetc/1410someprojetcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.1410someprojetc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 1410someprojetcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
